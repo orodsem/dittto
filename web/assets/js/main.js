629ab148c9bc27e4abffb95abd3eef0ae527c14a
Byron Alfonso <balfonso@sparknettech.com>
2017-10-28 23:54:49 +0800

    Revamped the navigation; Added sliding sidebar; Added a couple of styling with the dashboard; Refactored a couple of stuff

---
+++ web/assets/js/main.js
@@ -0,0 +1,30 @@
+(function(){
+
+	$(document).ready(function(){
+		
+		$('.open-sidebar-btn').on('click', function(){			
+			openNav();
+			$(this).hide();
+		});
+
+		$('.close-sidebar-btn').on('click', function(){					
+			closeNav();
+			$('.open-sidebar-btn').show();
+		})
+
+
+	});
+
+	/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+	function openNav() {
+		$('#mySidenav').css('width', '250px');
+		$('.main-wrapper').css('margin-left', '250px');
+	}
+
+	/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+	function closeNav() {
+		$('#mySidenav').css('width', '0');
+		$('.main-wrapper').css('margin-left', '0');
+	}
+
+})();
