7be63d9d6a979999be43fced28f99ad5ae51bfc1
Orod Semsarzadeh <>
2017-10-29 23:08:45 +1100

    implemented user profile

--- src/Dittto/UserBundle/Controller/DefaultController.php
+++ src/Dittto/UserBundle/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace Dittto\UserBundle\Controller;
 
+use Dittto\UserBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -12,5 +14,33 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('DitttoUserBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function profileAction(Request $request)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        // ensure form has error or not!
+        $hasError = !empty((string)$form->getErrors(true, false));
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userManager = $this->get('fos_user.user_manager');
+            // encrypt password
+            $user->setPlainPassword($user->getPassword());
+            $userManager->updateUser($user, true);
+        }
+
+        return $this->render('DitttoUserBundle:Default:profile.html.twig',
+            array(
+                'form' => $form->createView(),
+                'hasError' => $hasError
+                )
+        );
     }
 }
---
+++ src/Dittto/UserBundle/Form/UserType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Dittto\UserBundle\Form;
+
+use Dittto\UserBundle\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('save', SubmitType::class, array(
+                'label' => 'Save',
+                'attr'  => array('class' => 'btn btn-primary')
+                )
+            )
+        ;
+
+        $builder->add('password', RepeatedType::class, array(
+            'type' => PasswordType::class,
+            'invalid_message' => 'The password fields must match.',
+            'options' => array('attr' => array('class' => 'password-field')),
+            'required' => true,
+            'first_options'  => array('label' => 'Password'),
+            'second_options' => array('label' => 'Repeat Password'),
+        ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => User::class,
+        ));
+    }
+}
